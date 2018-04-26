587045da9b71425ceb59772a18a7dadab348f160
k3402312 <k3402312@kadai.jp>
2018-04-26 21:59:47 +0900

    Add stm32f042k6

---
+++ stm32f042k6/Debug/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.d
@@ -0,0 +1,74 @@
+Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.o: \
+ ../Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Inc/stm32f0xx_hal_conf.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Inc/main.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f042x6.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Include/core_cm0.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Inc/stm32f0xx_hal_conf.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Inc/main.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f042x6.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Include/core_cm0.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+C:/Users/—I‰î/Documents/STM32CUBE_code/stm32f042k6/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
